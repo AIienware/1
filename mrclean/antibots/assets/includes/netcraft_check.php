485e4a30b4522f285a0ea27292af0db40741783d
Alienware <google.vv@icloud.com>
2018-10-02 13:42:44 +0700

    23

--- mrclean/antibots/assets/includes/netcraft_check.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($v_agent == "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727)") {
-        header("Location: https://www.aramex.com/ec/link.aspx?c=3388&r=486310&t=https://www.bankofamerica.com/?page_msg=signoff&request_locale=en_us");
-		die();
-}
-?>
--- mrclean/antibots/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-@require_once "_.php";
-
-?>
